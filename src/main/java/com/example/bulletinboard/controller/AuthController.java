2527c51fe604165cb011e22ae059875d719bac9f
Tsubasa Utsumi <utsumitsubasaneihaiyi@gmail.com>
2026-07-26 20:27:10 +0900

    新規登録画面の作成

--- src/main/java/com/example/bulletinboard/controller/AuthController.java
+++ src/main/java/com/example/bulletinboard/controller/AuthController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthController {
 
+   private final CustomUserDetailsService userDetailsService;
+
    //依存オブジェクトをインジェクション
-   public AuthController(CustomUserDetailsService userDetailsService){
+   public AuthController(CustomUserDetailsService userDetailsService, CustomUserDetailsService customUserDetailsService){
      this.userDetailsService = userDetailsService;
-   }
+     }
 
    //ログイン画面の表示
    @GetMapping("/login")
@@ -30,6 +32,13 @@ public class AuthController {
        model.addAttribute("user", new User());
        return "auth/register";
    }
+
+  //新規登録処理の実行
+  @PostMapping("/register")
+  public String register(@ModelAttribute User user){
+    userDetailsService.registerUser(user);
+    return "redirect:/login?register_success"; //登録後はログイン画面へ
+  }
 
 
 
