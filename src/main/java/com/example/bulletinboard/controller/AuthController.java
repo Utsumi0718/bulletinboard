9f84ddf4e4624602642f8f7627dfcf04ad5e36fa
Tsubasa Utsumi <utsumitsubasaneihaiyi@gmail.com>
2026-07-26 20:07:21 +0900

    認証用コントローラーの作成(AuthController.java)

---
+++ src/main/java/com/example/bulletinboard/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.example.bulletinboard.controller;
+
+import com.example.bulletinboard.model.User;
+import com.example.bulletinboard.service.CustomUserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class AuthController {
+
+   //依存オブジェクトをインジェクション
+   public AuthController(CustomUserDetailsService userDetailsService){
+     this.userDetailsService = userDetailsService;
+   }
+
+   //ログイン画面の表示
+   @GetMapping("/login")
+   public String login(){
+     return "auth/login";
+   }
+
+   //新規登録画面の表示
+   @GetMapping("/register")
+   public String resisterForm(Model model) {
+       model.addAttribute("user", new User());
+       return "auth/register";
+   }
+
+
+
+
+}
