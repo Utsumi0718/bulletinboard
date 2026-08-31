ba45940f799f3b318f9757d590b325616f13ba09
Tsubasa Utsumi <utsumitsubasaneihaiyi@gmail.com>
2026-08-31 22:48:26 +0900

    LoginSecurityをsecrityパッケージに移動

--- backend/src/test/java/com/example/bulletinboard/security/LoginSecurityTest.java
+++ backend/src/test/java/com/example/bulletinboard/security/LoginSecurityTest.java
@@ -1,4 +1,4 @@
-package com.example.bulletinboard;
+package com.example.bulletinboard.security;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
